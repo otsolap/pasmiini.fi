12c893c0b85bd2ea672a51ce1fd52e8087ad6177
OtsoLap <59653348+otsolap@users.noreply.github.com>
2022-12-17 17:20:51 +0200

    WIP hero component

---
+++ components/hero.js
@@ -0,0 +1,39 @@
+import styles from "../styles/Hero.module.scss";
+import Image from "next/image";
+import Link from "next/link";
+
+const Hero = ({ hero }) => {
+  return (
+    <section id={styles.hero}>
+      {hero.media == "image" &&
+        hero.image(
+          <div className={styles.imageContainer}>
+            <Image
+              src={hero.image}
+              alt={hero.title}
+              fill
+              class={styles.heroImage}
+              quality={100}
+            />
+          </div>
+        )}
+      <div className={styles.contentContainer}>
+        <div className={styles.content}>
+          {hero.title && <h1>{hero.title}</h1>}
+          {hero.description && <h3>{hero.description}</h3>}
+          {hero.buttons && (
+            <div className="buttonWrapper">
+              {hero.buttons.forEach((button) => {
+                <Link href={button.link}>
+                  <a className={`btn btn--hero`}>{button.title}</a>
+                </Link>;
+              })}
+            </div>
+          )}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
