940648697a29ee504bb8ba27a9b9eb64112ff346
OtsoLap <59653348+otsolap@users.noreply.github.com>
2022-12-17 17:21:22 +0200

    WIP on styles. Google fonts added.

--- pages/_document.js
+++ pages/_document.js
@@ -5,9 +5,9 @@ class MyDocument extends Document {
         return (
             <Html>
                 <Head>
-                    <link rel="preconnect" href="https://fonts.googleapis.com" />
-                    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-                    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet" />
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="true" />
+                <link href="https://fonts.googleapis.com/css2?family=Bitter:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet" />
                 </Head>
                 <body>
                     <Main />
@@ -20,3 +20,4 @@ class MyDocument extends Document {
 }
 
 export default MyDocument
+
