c5b0a235c7fd45618c4b485c382781a8807bf5d8
OtsoLap <59653348+otsolap@users.noreply.github.com>
2022-12-18 23:10:15 +0200

    refactored mediaMix column into a own partial file

---
+++ partials/mediaMixItem.js
@@ -0,0 +1,26 @@
+import styles from "../styles/components/mediaMix.module.scss";
+import Image from "next/image";
+import MarkdownBlock from "@partials/markdownBlock";
+import YoutubeEmbed from "./youtubeEmbed";
+
+const MediaMixItem = ({ type, image, video, text }) => {
+  return (
+    <>
+      {type == "image" && image && (
+        <div className={styles.mediaMixImageContainer}>
+          <Image
+            className={styles.mediaMixImage}
+            src={image}
+            alt=""
+            fill
+            quality={100}
+          />
+        </div>
+      )}
+      {type == "text" && text && <MarkdownBlock markdown={text} />}
+      {type == "video" && video && <YoutubeEmbed link={video} />}
+    </>
+  );
+};
+
+export default MediaMixItem;
