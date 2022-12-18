2b91e288e3da0e0ca278363222fba70bd4e04ac2
OtsoLap <59653348+otsolap@users.noreply.github.com>
2022-12-18 11:03:37 +0200

    make image component render conditionally

--- components/hero.js
+++ components/hero.js
@@ -5,21 +5,33 @@ import Link from "next/link";
 const Hero = ({ hero }) => {
   return (
     <section id={styles.hero}>
-      {hero.media == "image" &&
-        hero.image && (
-          <div className={styles.imageContainer}>
+      {hero.media == "image" && hero.image && (
+        <div className={styles.imageContainer}>
+          (hero.imageWidth && (
             <Image
               className={styles.heroImage}
               src={hero.image}
               alt={hero.title}
               quality={100}
-              fill={hero.imageWidth ? true : false}
-              {...hero.imageWidth ? '' : width=720}
-              {...hero.imageWidth ? false : height=625}
+              fill
             />
-          </div>
-        )}
-      <div className={`${styles.contentContainer} ${hero.imageWidth ? styles.bgTransparent : styles.bgDefault} `}>
+          ) : (
+            <Image
+              className={styles.heroImage}
+              src={hero.image}
+              alt={hero.title}
+              quality={100}
+              width={720}
+              height={625}
+            />
+          ))
+        </div>
+      )}
+      <div
+        className={`${styles.contentContainer} ${
+          hero.imageWidth ? styles.bgTransparent : styles.bgDefault
+        } `}
+      >
         <div className={styles.content}>
           {hero.title && <h1>{hero.title}</h1>}
           {hero.description && <h3>{hero.description}</h3>}
