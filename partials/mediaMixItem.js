d9a3e9ca137d80dd8d8e8ab5e5462f4da1bfec25
OtsoLap <59653348+otsolap@users.noreply.github.com>
2022-12-18 23:20:18 +0200

    added links and buttons to the mix

--- partials/mediaMixItem.js
+++ partials/mediaMixItem.js
@@ -6,6 +6,7 @@ import YoutubeEmbed from "./youtubeEmbed";
 const MediaMixItem = ({ type, image, video, text }) => {
   return (
     <div className={styles.mediaMixColumn}>
+      {type == "video" && video && <YoutubeEmbed link={video} />}
       {type == "image" && image && (
         <div className={styles.mediaMixImageContainer}>
           <Image
@@ -18,7 +19,24 @@ const MediaMixItem = ({ type, image, video, text }) => {
         </div>
       )}
       {type == "text" && text && <MarkdownBlock markdown={text} />}
-      {type == "video" && video && <YoutubeEmbed link={video} />}
+      {type == "text" && links && (
+        <ul className="linkWrapper">
+          {links.map((link, i) => (
+            <li key={i}>
+              <Link href={link.url}>{link.title}</Link>
+            </li>
+          ))}
+        </ul>
+      )}
+      {type == "text" && buttons && (
+        <div className="buttonWrapper">
+          {buttons.map((button, i) => (
+            <Link key={i} href={button.url} className={`btn`}>
+              {button.title}
+            </Link>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
