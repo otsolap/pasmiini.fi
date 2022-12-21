a4777877ae6a2c6ee53c98ec15a0e519e99e1ece
OtsoLap <59653348+otsolap@users.noreply.github.com>
2022-12-21 18:11:02 +0200

    problems with netlify cms again

--- pages/about.js
+++ pages/about.js
@@ -1,17 +1,8 @@
-import Layout from '../components/layout'
-import { attributes, html } from '../content/about.md'
 
 const About = () => (
-  <Layout>
-    <h1>{attributes.title}</h1>
-    <div dangerouslySetInnerHTML={{ __html: html }} />
-    <style jsx>{`
-      h1,
-      div {
-        text-align: center;
-      }
-    `}</style>
-  </Layout>
+<section>
+  Hello World
+</section>
 )
 
 export default About
